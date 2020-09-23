1d1273001bbe5123b46cc0886b66dae14d043ee2
Pavel Musienko <57036560+pqtech@users.noreply.github.com>
2020-09-23 04:23:27 +0300

    Add new test config files for ex3

--- src/main/java/setup/BaseTest.java
+++ src/main/java/setup/BaseTest.java
@@ -2,7 +2,10 @@ package setup;
 
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import pageObjects.PageObject;
 
 import java.io.File;
@@ -16,19 +19,28 @@ public class BaseTest implements IDriver {
     IPageObject po;
 
     @Override
-    public AppiumDriver getDriver() { return appiumDriver; }
+    public AppiumDriver getDriver() {
+        return appiumDriver;
+    }
 
     public IPageObject getPo() {
         return po;
     }
 
-    @Parameters({"platformName","appType","deviceName","browserName","app","udid"})
+    @Parameters({"platformName", "appType", "deviceName", "browserName", "app", "udid",
+            "appPackage", "appActivity", "bundleId"})
     @BeforeSuite(alwaysRun = true)
-    public void setUp(String platformName, String appType, String deviceName,
+    public void setUp(String platformName, String appType,
+                      @Optional("") String deviceName,
                       @Optional("") String browserName,
-                      @Optional("") String app, String udid) throws Exception {
-        System.out.println("Before: app type - "+appType);
-        setAppiumDriver(platformName, deviceName, browserName, app, udid);
+                      @Optional("") String app,
+                      @Optional("") String udid,
+                      @Optional("") String appPackage,
+                      @Optional("") String appActivity,
+                      @Optional("") String bundleId
+    ) throws Exception {
+        System.out.println("Before: app type - " + appType);
+        setAppiumDriver(platformName, deviceName, browserName, app, udid, appPackage, appActivity, bundleId);
         setPageObject(appType, appiumDriver);
 
     }
@@ -40,17 +52,26 @@ public class BaseTest implements IDriver {
     }
 
     private void setAppiumDriver(String platformName, String deviceName,
-                                 String browserName, String app, String udid){
+                                 String browserName, String app, String udid,
+                                 String appPackage, String appActivity, String bundleId) {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         //mandatory Android capabilities
-        capabilities.setCapability("platformName",platformName);
-        capabilities.setCapability("deviceName",deviceName);
-        capabilities.setCapability("udid",udid);
+        capabilities.setCapability("platformName", platformName);
+        capabilities.setCapability("deviceName", deviceName);
+        capabilities.setCapability("udid", udid);
 
-        if(app.endsWith(".apk")) capabilities.setCapability("app", (new File(app)).getAbsolutePath());
+        if (app.endsWith(".apk")) capabilities.setCapability("app", (new File(app)).getAbsolutePath());
 
         capabilities.setCapability("browserName", browserName);
-        capabilities.setCapability("chromedriverDisableBuildCheck","true");
+        capabilities.setCapability("chromedriverDisableBuildCheck", "true");
+
+        // Capabilities for test of Android native app on EPAM Mobile Cloud
+        capabilities.setCapability("appPackage", appPackage);
+        capabilities.setCapability("appActivity", appActivity);
+
+        // Capabilities for test of iOS native app on EPAM Mobile Cloud
+        capabilities.setCapability("bundleId", bundleId);
+
 
         try {
             appiumDriver = new AppiumDriver(new URL(System.getProperty("ts.appium")), capabilities);
