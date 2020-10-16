bb18cfa31c9f65e6a8ab5598cdbfb19512c5783f
Pavel Musienko <57036560+pqtech@users.noreply.github.com>
2020-10-16 21:22:00 +0300

    Delete platformName usage

--- src/test/java/scenarios/nativeMobileTests.java
+++ src/test/java/scenarios/nativeMobileTests.java
@@ -1,20 +1,15 @@
 package scenarios;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import pageObjects.NativePageObject;
 import setup.BaseTest;
 
 public class nativeMobileTests extends BaseTest {
 
     @Parameters({"userEmail", "userPassword", "budgetPageName"})
     @Test(groups = {"native"}, description = "Sign in with a registered account")
-    public void SignInAccountNativeTest(String userEmail, String userPassword,
-                                        String budgetPageName, String platformName)
+    public void SignInAccountNativeTest(String userEmail, String userPassword, String budgetPageName)
             throws IllegalAccessException, NoSuchFieldException, InstantiationException {
 
         // Perform signing in actions
@@ -22,19 +17,9 @@ public class nativeMobileTests extends BaseTest {
         getPo().getWelement("signInPasswordField").sendKeys(userPassword);
         getPo().getWelement("signInBtn").click();
 
-        if (platformName.equals("Android")) {
-            // Making sure that the Budged page has loaded
-            new WebDriverWait(getDriver(), 1)
-                    .until(ExpectedConditions.presenceOfElementLocated(By
-                            .xpath(NativePageObject.getActionBarTextLocator())));
-
-            // Get Budget page's name
-            String actualPageName = getPo().getWelement("actionBarText").getText();
-            Assert.assertEquals(actualPageName, budgetPageName);
-        } else if (platformName.equals("iOS")) {
-            Assert.assertTrue(getPo().getWelement("actionBar").isDisplayed());
-        }
-
+        // Get Budget page's name
+        String actualPageName = getPo().getWelement("actionBarText").getText();
+        Assert.assertEquals(actualPageName, budgetPageName);
         System.out.println("Sign in with an account native test done");
     }
 }
